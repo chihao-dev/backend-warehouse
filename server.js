b9c63c91a2bea4c9375dd77ae726e25c434fbc88
ChiHao <chihao@example.com>
2026-01-19 20:49:53 +0700

    Quản lý kho hàng

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/uploads', express.static('uploads'));
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
 });
